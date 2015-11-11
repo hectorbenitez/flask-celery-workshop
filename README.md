1d165a9f204ee96462d1b52731d0c5f189b5a8fb
hectorbenitez <hbenitez@nearsoft.com>
2015-11-10 21:25:05 -0600

    Send mail using Celery with Backend

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # flask-celery-workshop
+
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from celery import Celery
+from celery.result import AsyncResult
 from flask import Flask, request, render_template, session, redirect, url_for
 from tasks import send_mail_async
 
@@ -7,9 +8,10 @@ app.config['SECRET_KEY'] = 'top-secret!'
 
 # Celery configuration
 app.config['CELERY_BROKER_URL'] = 'amqp://localhost:5672'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
 
 # Initialize Celery
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery = Celery(app.name)
 celery.conf.update(app.config)
 
 
@@ -19,9 +21,25 @@ def index():
         return render_template('index.html')
 
     email = request.form['email']
-    send_mail_async.delay(email)
+    async_result = send_mail_async.delay(email)
+    print async_result.id
 
     return redirect(url_for('index'))
+
+
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    if request.method == 'GET':
+        return render_template('result.html')
+
+    result_id = request.form['result_id']
+    async_result = celery.AsyncResult(result_id)
+    print async_result.status
+    print async_result.ready()
+    print async_result.failed()
+    print async_result.successful()
+
+    return redirect(url_for('result'))
 
 if __name__ == '__main__':
     app.run(debug=True)
